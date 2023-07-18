a7decbb63913a52c62ba4d66fbcafb452c8e9702
Ewelti <mischiefmao@gmail.com>
2023-07-18 15:19:38 -0600

    adding site level lines to common species trends analyses

--- R/Common_spp_trends.R
+++ R/Common_spp_trends.R
@@ -210,6 +210,10 @@ points(x=si$spp_mm[si$site=="KiO3"], y=si$Value[si$site=="KiO3"], pch=23, bg=alp
 points(x=si$spp_mm[si$site=="KiW1"], y=si$Value[si$site=="KiW1"], pch=24, bg=alpha(4,0.6),col=alpha(4,0.6),lwd=2,cex=2.5)
 legend("bottomright", legend=c("Auba","Bieb","O3","W1"),col=c(1,2,3,4),pt.bg=c(1,2,3,4),pt.lwd=1, pch=c(21,22,23,24),lty=0,lwd=2,bty="n",pt.cex=2.5, cex=1.5)
 abline(lm(si$Value ~ si$spp_mm), lwd=2)
+abline(lm(si$Value[si$site=="Aubach"] ~ si$spp_mm[si$site=="Aubach"]),lwd=2,col=alpha(1,0.6),lty=2)
+abline(lm(si$Value[si$site=="Bieber"] ~ si$spp_mm[si$site=="Bieber"]),lwd=2,col=alpha(2,0.6),lty=2)
+abline(lm(si$Value[si$site=="KiO3"] ~ si$spp_mm[si$site=="KiO3"]),lwd=2,col=alpha(3,0.6),lty=2)
+abline(lm(si$Value[si$site=="KiW1"] ~ si$spp_mm[si$site=="KiW1"]),lwd=2,col=alpha(4,0.6),lty=2)
 
 dev.off()
 
